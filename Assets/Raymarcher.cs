aa4bc59622cc07a5249a57b3778f9652dfeedd38
Garrett Gunnell <gunnellg@oregonstate.edu>
2022-02-07 14:28:13 -0800

    Add empty camera script

---
+++ Assets/Raymarcher.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Camera))]
+public class Raymarcher : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
